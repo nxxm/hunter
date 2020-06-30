89635e15f7521753987f8faff41348657acda9cb
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2020-06-30 22:01:24 +0200

    :gear: try to use cmake official find-package.

--- cmake/projects/Boost/hunter.cmake
+++ cmake/projects/Boost/hunter.cmake
@@ -431,6 +431,12 @@ if(MSVC)
   )
 endif()
 
+hunter_cmake_args(
+    Boost
+    CMAKE_ARGS
+        USE_CONFIG_FROM_BOOST=ON
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_boost)
 hunter_cacheable(Boost)
 hunter_download(PACKAGE_NAME Boost PACKAGE_INTERNAL_DEPS_ID "48")
